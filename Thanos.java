c43e0e3973eaf76009676c6d3af8613adf6491b2
Alexandre Prieur <replay_76230@hotmail.fr>
2019-10-09 10:17:39 +0200

    ArrayListCorrection

--- Thanos.java
+++ Thanos.java
@@ -5,49 +5,62 @@ public class Thanos {
     public static void main(String[] args) {
 
         // TODO 1 : Create an empty heroes list
-        ArrayList<String> hero = new ArrayList<String>();
+        List<Hero> heroes = new ArrayList<>();
         // TODO 2 : Add those heroes to the list
         // name: Black Widow, age: 34
         Hero Black = new Hero("Black Widow", 34);
-        hero.add("Black Widow");
+        heroes.add(Black);
         // name: Captain America, age: 100
         Hero Captain = new Hero("Captain America", 100);
-        hero.add("Captain America");
+        heroes.add(Captain);
         // name: Vision, age: 3
         Hero Vision = new Hero("Vision", 3);
-        hero.add("Vision");
+        heroes.add(Vision);
         // name: Iron Man, age: 48
         Hero Iron = new Hero("Iron Man", 48);
-        hero.add("Iron Man");
+        heroes.add(Iron);
         // name: Scarlet Witch, age: 29
         Hero Scarlet = new Hero("Scarlet Witch", 29);
-        hero.add("Scarlet Witch");
+        heroes.add(Scarlet);
         // name: Thor, age: 1500
         Hero Thor = new Hero("Thor", 1500);
-        hero.add("Thor");
+        heroes.add(Thor);
         // name: Spider-Man, age: 18
         Hero Spider= new Hero("Spider-Man", 18);
-        hero.add("Spider-Man");
+        heroes.add(Spider);
         // name: Hulk, age: 49
         Hero Hulk = new Hero("Hulk", 49);
-        hero.add("Hulk");
+        heroes.add(Hulk);
         // name: Doctor Strange, age: 42
         Hero Doctor = new Hero("Doctor Strange", 42);
-        hero.add("Doctor Strange");
+        heroes.add(Doctor);
 
         // TODO 3 : It's Thor birthday, now he's 1501
         Thor.setAge(1501);
         // TODO 4 : Shuffle the heroes list
-        Collections.shuffle(hero);
+        Collections.shuffle(heroes);
         // TODO 5 : Keep only the half of the list
-        System.out.println("Original arrlist: " + hero);
-        List<String> hero2 = hero.subList(0, 4);
-        System.out.println("Sublist of arrlist: " + hero2);
+        System.out.println("");
+        System.out.println("Original arrlist: ");
+        for (Hero hero : heroes) {
+            System.out.print(hero.getName() + " ");
+            System.out.println("");
+        }
+        heroes = heroes.subList(0, 4);
+        System.out.println("");
+        System.out.println("Sublist of arrlist: ");
+        for (Hero hero2 : heroes) {
+            System.out.print(hero2.getName() + " ");
+            System.out.println("");
+        }
         // TODO 6 : Loop throught the list and display the name of the remaining heroes
-        for(int i = 0; i < hero2.size(); i++){
-            System.out.println(hero.get(i));
+        System.out.println("");
+        System.out.println("Remaining heroes: ");
+        for (Hero Hero : heroes) {
+            System.out.println(Hero.getName());
         }
         // TODO 7 : Display the age of Thor
+        System.out.println("");
         System.out.println("Happy birthday Thor, you are know " + Thor.getAge() + "! 🎉");
     }
 }
